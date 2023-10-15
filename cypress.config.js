c0ada90102478bf2a5fd46d503cc31b735e763f7
Filipe Conde <filipecondep@gmail.com>
2023-10-15 16:54:20 -0300

    setup: setup cypress run preferences

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    watchForFileChanges: false,
+    chromeWebSecurity: false,
+    screenshotOnRunFailure: false,
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
